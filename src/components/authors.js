a9134fae9567a99f3badd7c522a4b1612161313a
Tetyana <tetyana.lesivtsiv@gmail.com>
2021-09-02 10:11:54 +0300

    modified folder src/index.js and src/components/authors.js

--- src/components/authors.js
+++ src/components/authors.js
@@ -1,7 +1,7 @@
 const avatar1 =
   'https://upload.wikimedia.org/wikipedia/en/thumb/7/74/Anakin-Jedi.jpg/220px-Anakin-Jedi.jpg';
 const avatar2 =
-  'https://www.dynamomania.com/images/store/galleries/0122/4627/post581948_b04d3_content.jpg';
+  'https://pbs.twimg.com/media/E7xxQMZWQAQUPz1?format=jpg&name=small';
 const avatar3 =
   'https://upload.wikimedia.org/wikipedia/sco/thumb/7/7a/Manchester_United_FC_crest.svg/1200px-Manchester_United_FC_crest.svg.png';
 
@@ -12,9 +12,9 @@ export const authors = [
     avatar: avatar3,
   },
   {
-    name: 'Andriy Shevchenko',
+    name: 'EspresoTV',
     avatar: avatar2,
-    nickname: '@jksheva7',
+    nickname: '@EspresoTV',
   },
   {
     name: 'Anakin skywalker',
